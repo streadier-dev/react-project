9c7b98698d71f994e429e5df33f91b0d46d58c3b
gabriel <gabriel.faure@readinessit.com>
2020-02-27 11:49:30 -0300

    Badge y modificaciones en el index

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -7,10 +7,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Badge from './components/Badge';
+import './global.css';
 
-const jsx = <h1>Hello, Platzi Badges!</h1>;
-const element = React.createElement('a', {href: 'https://platzi.com'},'ir a el link')
+//const jsx = <h1>Hello, Platzi Badges!</h1>;
+//const element = React.createElement('a', {href: 'https://platzi.com'},'ir a el link')
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge firstName="Gabo" lastName="Faure" jobTitle= "Frontend Engineer" gitHub= "gabo_devops" />, container);
