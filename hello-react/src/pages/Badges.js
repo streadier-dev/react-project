6d22a9214afbf0ce864abdd187d781675c289988
gabriel <gabriel.faure@readinessit.com>
2020-02-29 21:01:33 -0300

    ciclo de vida implementado en consola de react

--- hello-react/src/pages/Badges.js
+++ hello-react/src/pages/Badges.js
@@ -5,9 +5,23 @@ import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
 import { Link } from 'react-router-dom';
 class Badges extends Component {
-    state = {
-        data: [
-            {
+
+    constructor (props){
+        super(props);
+        console.log('1. constructor()');
+
+        this.state = {
+        data: [],
+    };
+
+    }
+    componentDidMount(){
+        console.log('3. componentDidMount()');
+
+        this.timeoutId = setTimeout(() => {
+            this.setState({
+                data:[
+                           {
                 id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
                 firstName: 'Freda',
                 lastName: 'Grady',
@@ -37,10 +51,28 @@ class Badges extends Component {
                 avatarUrl:
                     'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
             },
-        ],
-    };
+                ],
+            })
+        }, 3000)
+    }
+componentDidUpdate(prevProps , prevState){
+    console.log('5. componentDidUpdate()');
+    console.log({
+        prevProps: prevProps,
+        prevState: prevState,
+    });
+    console.log({
+        props: this.props,
+        state: this.state,
+    });
+}
 
+componentWillUnmount(){
+    console.log('6. componentWillUnmount()');
+    clearTimeout(this.timeoutId);
+}
     render() {
+        console.log('2. render()')
         return (
             <React.Fragment>
                 <div className="Badges">
