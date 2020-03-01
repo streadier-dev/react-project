02e67014bc1df0a59bfc087a97f79d1a912b5fb6
gabriel <gabriel.faure@readinessit.com>
2020-03-01 13:43:11 -0300

    fix de lista avatar en BadgeNew

--- hello-react/src/api.js
+++ hello-react/src/api.js
@@ -28,7 +28,8 @@ const api = {
            return callApi('/badges');
         },
         create(badge) {
-            return callApi(`/badges`, {
+           // throw new Error('500 : Server internal Error');
+          return callApi(`/badges`, {
                 method: 'POST',
                 body: JSON.stringify(badge),
             });
--- hello-react/src/pages/BadgeNew.js
+++ hello-react/src/pages/BadgeNew.js
@@ -6,8 +6,11 @@ import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm'
 import api from '../api'
 import md5 from 'md5';
+import PageLoading from '../components/PageLoading';
 class BadgeNew extends React.Component {
   state = {
+    loading: false,
+    error: null,
     form: {
       firstName: '',
       lastName: '',
@@ -34,12 +37,17 @@ class BadgeNew extends React.Component {
     try {
       await api.badges.create(this.state.form)
       this.setState({ loading: false })
+
+      this.props.history.push('/badges');
     } catch (error) {
       this.setState({ loading: false, error: error })
     }
   };
 
   render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
@@ -68,6 +76,7 @@ class BadgeNew extends React.Component {
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
                 formValues={this.state.form}
+                error ={this.state.error}
               />
             </div>
           </div>
